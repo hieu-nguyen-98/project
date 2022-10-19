200460842db8ac3c62c7ad4925050429026de999
hieu-nguyen-98 <88076506+hieu-nguyen-98@users.noreply.github.com>
2022-10-19 11:17:20 +0700

    first commit

---
+++ database/migrations/2022_10_19_033917_create_orders_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('status');
+            $table->string('payment_method');
+            $table->string('payment_status');
+            $table->string('payment_id');
+            $table->string('total_price');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('city');
+            $table->string('postal_code');
+            $table->string('country');
+            $table->string('shipping_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
